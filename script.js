7a3b62fc4fa939639fe5cc7a82f9af9625697ebe
viacomeb <come.bertin@gmail.com>
2024-12-06 01:19:51 +0100

    vagues pas cassées

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('coeur').addEventListener('click', function() {
-    alert("Le cœur humain et les courants marins : Le cœur pompe le sang tout comme les courants marins régulent la circulation de l'eau dans l'océan.");
-  });
-  
-  document.getElementById('poumons').addEventListener('click', function() {
-    alert("Les poumons et les échanges gazeux : Tout comme les poumons échangent des gaz, l'océan absorbe et relâche du dioxyde de carbone.");
-  });
-  
-  // Ajouter plus d'interactions pour les autres éléments  
